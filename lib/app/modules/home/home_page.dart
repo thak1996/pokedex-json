d41e9ccf491d42be033bf957ea3f9327db2b137a
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2024-11-07 18:52:10 -0300

    ajustando exibição adequada e componetização do card principal

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,9 +1,8 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:integration/app/modules/home/widgets/pokemon_card.dart';
 import 'package:provider/provider.dart';
-import '../../core/models/pokemon_model.dart';
 import '../../core/services/pokemon_service.dart';
 import '../../core/theme/app_styles.dart';
 import '../../core/theme/theme_controller.dart';
@@ -25,51 +24,53 @@ class HomePage extends StatelessWidget {
           endDrawer: const EndDrawer(),
           body: Stack(
             children: [
-              ColorFiltered(
-                colorFilter: ColorFilter.mode(
-                  controller.themeController.themeMode == ThemeMode.dark
-                      ? Colors.white
-                      : Colors.grey[300]!,
-                  BlendMode.srcIn,
-                ),
-                child: Image.asset(
-                  'assets/images/pokeball.png',
-                  height: 300.h,
-                  fit: BoxFit.fitHeight,
-                ),
-              ),
-              Text(
-                'Pokedex',
-                style: AppStyles.headerStyles[Header.headline],
-              ),
-              Builder(
-                builder: (context) => IconButton(
-                  icon: Icon(
-                    Icons.menu,
-                    size: 30,
-                    color:
-                        controller.themeController.themeMode == ThemeMode.dark
-                            ? Colors.white
-                            : Colors.black,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/pokeball.png',
+                        height: 50.h,
+                        fit: BoxFit.fitHeight,
+                      ),
+                      Text(
+                        'Pokedex',
+                        style: AppStyles.headerStyles[Header.headline]
+                            ?.apply(color: Colors.white),
+                      ),
+                      Builder(
+                        builder: (context) => IconButton(
+                          icon: const Icon(
+                            Icons.menu,
+                            size: 30,
+                            color: Colors.white,
+                          ),
+                          onPressed: () => Scaffold.of(context).openEndDrawer(),
+                        ),
+                      ),
+                    ],
                   ),
-                  onPressed: () => Scaffold.of(context).openEndDrawer(),
-                ),
+                ],
               ),
               Padding(
                 padding: EdgeInsets.only(
-                  top: 200.h,
-                  left: 2.w,
-                  right: 2.w,
-                  bottom: 2.h,
+                  top: 100.h,
+                  left: 6.w,
+                  right: 6.w,
+                  bottom: 6.h,
                 ),
                 child: Container(
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.r),
-                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(6.r),
                     boxShadow: AppStyles.outerShadow,
+                    color:
+                        controller.themeController.themeMode == ThemeMode.dark
+                            ? Colors.black
+                            : Colors.white,
                   ),
                   child: SafeArea(
-                    child: Expanded(child: _buildContent(controller)),
+                    child: _buildContent(controller),
                   ),
                 ),
               ),
@@ -91,110 +92,21 @@ class HomePage extends StatelessWidget {
       default:
         final pokemons = controller.pokemons;
         return GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 3,
             childAspectRatio: 1,
+            mainAxisSpacing: 4.h,
+            crossAxisSpacing: 4.w,
           ),
           itemCount: pokemons.length,
           itemBuilder: (context, index) {
             final pokemon = pokemons[index];
-            final typesOne = pokemon.type.first;
-            final number = pokemon.id.toString().padLeft(3, '0');
-            return Padding(
-              padding: EdgeInsets.only(
-                left: 4.w,
-                right: 4.w,
-                bottom: 4.h,
-                top: 4.h,
-              ),
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.r),
-                ),
-                color: controller.getCardBackgroundColor(typesOne),
-                child: InkWell(
-                  borderRadius: BorderRadius.circular(20.r),
-                  onTap: () => Modular.to.pushNamed('/details/${pokemon.id}'),
-                  child: Stack(
-                    children: [
-                      imgPokeballCard(),
-                      imgPokemonCard(pokemon),
-                      namePokemonCard(pokemon),
-                      idPokemonCard(number),
-                    ],
-                  ),
-                ),
-              ),
+            return PokemonCard(
+              onTap: () => Modular.to.pushNamed('/details/${pokemon.id}'),
+              pokemon: pokemon,
             );
           },
         );
     }
-  }
-
-  Widget idPokemonCard(String number) {
-    return Align(
-      alignment: Alignment.topRight,
-      child: Padding(
-        padding: EdgeInsets.only(right: 8.w, top: 8.h),
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 2.h),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20.r),
-            color: Colors.black38,
-          ),
-          child: Text(
-            '#$number',
-            style: AppStyles.bodyStyles[Body.caption]?.copyWith(
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget imgPokeballCard() {
-    return Align(
-      alignment: Alignment.center,
-      child: Opacity(
-        opacity: 0.3,
-        child: Image.asset(
-          'assets/images/pokeball.png',
-          fit: BoxFit.fitHeight,
-        ),
-      ),
-    );
-  }
-
-  Widget imgPokemonCard(Pokemon pokemon) {
-    return Align(
-      alignment: Alignment.center,
-      child: CachedNetworkImage(
-        imageUrl: pokemon.img,
-        height: 100.h,
-        fit: BoxFit.fitHeight,
-      ),
-    );
-  }
-
-  Widget namePokemonCard(Pokemon pokemon) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 1.h),
-        margin: EdgeInsets.only(bottom: 4.h),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.r),
-          color: Colors.black38,
-        ),
-        child: Text(
-          pokemon.name,
-          style: AppStyles.bodyStyles[Body.caption]?.copyWith(
-            color: Colors.white,
-          ),
-        ),
-      ),
-    );
   }
 }
---
+++ lib/app/modules/home/widgets/pokemon_card.dart
@@ -0,0 +1,92 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../core/models/pokemon_model.dart';
+import '../../../core/models/pokemon_type.dart';
+import '../../../core/theme/app_styles.dart';
+
+class PokemonCard extends StatelessWidget {
+  const PokemonCard({
+    super.key,
+    required this.pokemon,
+    required this.onTap,
+  });
+
+  final Function()? onTap;
+  final Pokemon pokemon;
+
+  @override
+  Widget build(BuildContext context) {
+    final typesOne = pokemon.type.first;
+    final number = pokemon.id.toString().padLeft(3, '0');
+    return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.r),
+      ),
+      color: PokemonTypeInfo.getColor(typesOne).withOpacity(0.7),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(20.r),
+        onTap: onTap,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: Opacity(
+                opacity: 0.3,
+                child: Image.asset(
+                  'assets/images/pokeball.png',
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: CachedNetworkImage(
+                imageUrl: pokemon.img,
+                height: 100.h,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 1.h),
+                margin: EdgeInsets.only(bottom: 4.h),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.r),
+                  color: Colors.black38,
+                ),
+                child: Text(
+                  pokemon.name,
+                  style: AppStyles.bodyStyles[Body.caption]?.copyWith(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: EdgeInsets.only(right: 8.w, top: 8.h),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 2.h),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.r),
+                    color: Colors.black38,
+                  ),
+                  child: Text(
+                    '#$number',
+                    style: AppStyles.bodyStyles[Body.caption]?.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
